8e0a39ad9281c8e9fad92e2b69818db14764df33
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-17 20:32:20 -0300

    Update app.py

--- true website/backend/app.py
+++ true website/backend/app.py
@@ -4,17 +4,7 @@ app = Flask(__name__)
 
 DATABASE = 'users.db'
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    return render_template("login.html")
-
-@app.route("/create_acc")
-def create_acc():
-    return render_template("create_acc.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
