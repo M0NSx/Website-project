9160b5b075c6570247e7036dc35d2714341e9950
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-16 23:31:23 -0300

    Update app.py

--- true website/backend/app.py
+++ true website/backend/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, redirect, request, flash, url_or
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route("/")
-@app.route("/index")
 def index():
