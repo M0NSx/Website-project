170d7d31606217a791e4e7dbd8d8f384f59f4da8
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-17 20:33:06 -0300

    Update app.py

--- true website/backend/app.py
+++ true website/backend/app.py
@@ -7,6 +7,9 @@ DATABASE = 'users.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
 
 if __name__ == "__main__":
     app.run(debug=True)
