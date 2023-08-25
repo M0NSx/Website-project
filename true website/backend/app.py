17b9676bd71cfdeca9d938e5220437a355efb956
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-24 21:11:29 -0300

    Update app.py

--- true website/backend/app.py
+++ true website/backend/app.py
@@ -19,6 +19,7 @@ class Combo(db.Model):
     fighting_style = db.Column(db.String(50), nullable=False)
     sword = db.Column(db.String(50), nullable=False)
     gun = db.Column(db.String(50), nullable=False)
+    combo_name = db.Column(db.String(100), nullable=False)
 
 
 @app.route("/")
