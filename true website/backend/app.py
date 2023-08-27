20a623256b09bac1bb8907e8e6b42dab99f80525
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-26 23:49:01 -0300

    Update app.py

--- true website/backend/app.py
+++ true website/backend/app.py
@@ -122,6 +122,7 @@ def edit_combo(id):
         new_fruit = request.form['fruit']
         new_fighting_style = request.form['fighting_style']
         new_sword = request.form['sword']
+        new_gun = request.form['gun']
 
 
 if __name__ == '__main__':
