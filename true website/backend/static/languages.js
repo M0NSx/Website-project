d8977470324f4065dc3342218de61443c76f1609
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-01 20:19:43 -0300

    Create languages.js

---
+++ true website/backend/static/languages.js
@@ -0,0 +1,12 @@
+// Português
+$(".br").click(function () {
+    // Header
+    $(".nav").children().eq(1).text("Dashboard");
+    $(".nav").children().eq(2).text("Entrar");
+    $(".nav").children().eq(3).text("Criar Conta");
+    $(".language-selected").text("pt-BR");
+    $(".language-selected").removeClass("change-en change-fr change-kr");
+    $(".language-selected").addClass("change-br");
+    // Get Started
+    $(".get-started").text("Comece Já");
+});
