a8d58bd2b9c9f5500051e313beeecd56b511e2ad
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-01 20:20:10 -0300

    Update languages.js

--- true website/backend/static/languages.js
+++ true website/backend/static/languages.js
@@ -10,3 +10,42 @@ $(".br").click(function () {
     // Get Started
     $(".get-started").text("Comece Já");
 });
+
+// English
+$(".en").click(function () {
+    // Header
+    $(".nav").children().eq(1).text("Dashboard");
+    $(".nav").children().eq(2).text("Log In");
+    $(".nav").children().eq(3).text("Create Account");
+    $(".language-selected").text("en-US");
+    $(".language-selected").removeClass("change-br change-fr change-kr");
+    $(".language-selected").addClass("change-en");
+    // Get Started
+    $(".get-started").text("Get Started");
+});
+
+// Français
+$(".fr").click(function () {
+    // Header
+    $(".nav").children().eq(1).text("Dashboard");
+    $(".nav").children().eq(2).text("Entrer");
+    $(".nav").children().eq(3).text("Crée Compte");
+    $(".language-selected").text("FR");
+    $(".language-selected").removeClass("change-br change-en change-kr");
+    $(".language-selected").addClass("change-fr");
+    // Get Started
+    $(".get-started").text("Commencer");
+});
+
+// 한국어
+$(".kr").click(function () {
+    // Header
+    $(".nav").children().eq(1).text("대시보드");
+    $(".nav").children().eq(2).text("로그인");
+    $(".nav").children().eq(3).text("계정 생성");
+    $(".language-selected").text("KR");
+    $(".language-selected").removeClass("change-br change-en change-fr");
+    $(".language-selected").addClass("change-kr");
+    // Get Started
+    $(".get-started").text("시작");
+});
