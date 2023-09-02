7999627babc4f25c5635e4b0539e9ac3487ccd46
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-02 18:49:10 -0300

    Update languagesabout.js

--- true website/backend/static/languagesabout.js
+++ true website/backend/static/languagesabout.js
@@ -40,3 +40,4 @@ $(".fr").click(function () {
     // Header
     $("#nav ul li:nth-child(1) a").html('<i class="bi bi-book" aria-hidden="true"></i> À Propos');
     $("#nav ul li:nth-child(2) a").html('<i class="bi bi-plus-square" aria-hidden="true"></i> Créer Combo');
+    $("#nav ul li:nth-child(3) a").html('<i class="bi bi-journals" aria-hidden="true"></i> Voir Combo');
