5f6c65a58b210782491c7c8a804322c5b8974e69
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-02 18:46:34 -0300

    Update languagesabout.js

--- true website/backend/static/languagesabout.js
+++ true website/backend/static/languagesabout.js
@@ -22,3 +22,4 @@ $(".en").click(function () {
     $("#nav ul li:nth-child(1) a").html('<i class="bi bi-book" aria-hidden="true"></i> About');
     $("#nav ul li:nth-child(2) a").html('<i class="bi bi-plus-square" aria-hidden="true"></i> Create Your Combo');
     $("#nav ul li:nth-child(3) a").html('<i class="bi bi-journals" aria-hidden="true"></i> View Combo');
+    $("#nav ul li:nth-child(4) a").html('<i class="bi bi-people" aria-hidden="true"></i> Community');
