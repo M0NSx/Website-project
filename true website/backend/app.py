d683578a9973fd9af6e64588fd8be38340cb574c
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-09-05 21:29:31 -0300

    Update app.py

--- true website/backend/app.py
+++ true website/backend/app.py
@@ -249,6 +249,7 @@ def load_more_combos():
     return render_template('combo_list.html', combos=combos)
 
 @app.route('/profile', methods=["POST", "GET"])
+def profile():
 
 
 if __name__ == '__main__':
